7496811fc13f38246e9ea0abf1b67b77c805a512
doyu <ydh0905@nate.com>
2022-12-28 22:39:18 +0900

    data transformation(select)

--- src/pages/RQSuperHeroes.jsx
+++ src/pages/RQSuperHeroes.jsx
@@ -1,29 +1,20 @@
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
-import { useState } from 'react';
 
 const fetchSuperHeroes = () => {
   return axios.get('http://localhost:4000/superheroes');
 };
 
 export default function RQSuperHeroes() {
-  const [interval, setInterval] = useState(3000);
-
-  const onSuccess = ({ data }) => {
-    if (data.length >= 4) {
-      setInterval(false);
-    } else {
-      setInterval(3000);
-    }
+  const onSuccess = (data) => {
+    console.log('Perform side effect after data fetching', data);
   };
   const onError = (error) => {
-    if (error) {
-      setInterval(false);
-    }
+    console.log('Perform side effect after encountering error', error);
   };
 
   const {
-    data: heroes,
+    data: heroNames,
     isLoading,
     isFetching,
     isError,
@@ -37,9 +28,13 @@ export default function RQSuperHeroes() {
     // refetchInterval: 2000,
     // refetchIntervalInBackground: true,
     // enabled: false,
-    refetchInterval: interval,
+    // refetchInterval: interval,
     onSuccess,
     onError,
+    select: ({ data }) => {
+      const heroNames = data.map((hero) => hero.name);
+      return heroNames;
+    },
   });
 
   console.log({ isLoading, isFetching });
@@ -52,12 +47,17 @@ export default function RQSuperHeroes() {
     return <h2>{error.message}</h2>;
   }
 
+  console.log(heroNames);
+
   return (
     <>
       <h2>RQSuperHeroes Page</h2>
       <button onClick={refetch}>Fetch heroes</button>
-      {heroes?.data.map((hero) => (
+      {/* {heroes?.data.map((hero) => (
         <div key={hero.id}>{hero.name}</div>
+      ))} */}
+      {heroNames.map((heroName) => (
+        <div key={heroName}>{heroName}</div>
       ))}
     </>
   );
